fa123956ee9720c8fc2c0fb2b1e88ca5fbb6a956
robinwebber <robinewebber@gmail.com>
2019-08-22 16:36:53 +0000

    admin/categories implemented

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,6 +1,49 @@
 class Admin::CategoriesController < ApplicationController
 
   def index
-    @categories = Category.order(id: :desc).all
+    @categories = Category.order(id: :asc).all
   end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      redirect_to [:admin, :categories], notice: 'Category created!'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
+
 end
+
+
+# def product_params
+#   params.require(:product).permit(
+#     :name,
+#     :description,
+#     :category_id,
+#     :quantity,
+#     :image,
+#     :price
+#   )
+# end
+
+# def create
+#   @product = Product.new(product_params)
+
+#   if @product.save
+#     redirect_to [:admin, :products], notice: 'Product created!'
+#   else
+#     render :new
+#   end
+# end
