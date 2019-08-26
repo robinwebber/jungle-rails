08c616dac24afe33fa4e2083e025cc8e9b3c3640
robinwebber <robinewebber@gmail.com>
2019-08-26 20:45:16 +0000

    rspec testing suite added

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,13 @@ Rails.application.routes.draw do
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
 
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
   get '/about', to: 'about#show'
 
   resource :cart, only: [:show] do
